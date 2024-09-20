cf04ea648d274557a134086354fd8fbd8ea9f73e
oneul0 <neul.dev@gmail.com>
2024-09-20 17:16:06 +0900

    Chore: 버그발견

---
+++ src/main/java/fi/re/firebackend/controller/forex/ForexController.java
@@ -0,0 +1,12 @@
+package fi.re.firebackend.controller.forex;
+
+import fi.re.firebackend.service.forex.ForexService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/forex")
+public class ForexController {
+//    private final ForexService;
+
+}
---
+++ src/main/java/fi/re/firebackend/service/forex/ForexService.java
@@ -0,0 +1,9 @@
+package fi.re.firebackend.service.forex;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ForexService {
+    void setForexFromApi();
+
+}
