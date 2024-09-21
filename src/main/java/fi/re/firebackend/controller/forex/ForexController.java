896510f4522fb2c2abb7cb87493deed2298cca05
oneul0 <neul.dev@gmail.com>
2024-09-21 18:35:53 +0900

    Feat: make forex

--- src/main/java/fi/re/firebackend/controller/forex/ForexController.java
+++ src/main/java/fi/re/firebackend/controller/forex/ForexController.java
@@ -1,12 +1,23 @@
 package fi.re.firebackend.controller.forex;
 
 import fi.re.firebackend.service.forex.ForexService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/forex")
 public class ForexController {
-//    private final ForexService;
 
+
+    private final ForexService forexService;
+
+    public ForexController(ForexService forexService) {
+        this.forexService = forexService;
+    }
+
+    @GetMapping("/forexInfo")
+    public List<ForexInfo> forexInfo() {
+        
+    }
 }
