d63e7e46664aef7aac5a57dd50544fdfe4d08227
kUZEEwon <pink7519@pusan.ac.kr>
2024-09-24 11:06:01 +0900

    local 변경 사항 저장

--- src/main/java/fi/re/firebackend/security/SecurityConfig.java
+++ src/main/java/fi/re/firebackend/security/SecurityConfig.java
@@ -42,9 +42,9 @@ public class SecurityConfig {
                 .addFilterBefore(this.corsFilter(), UsernamePasswordAuthenticationFilter.class)
 
                 .authorizeRequests(authorizeHttpRequests -> authorizeHttpRequests
-                        .antMatchers("/naver/**").permitAll()
-                        .antMatchers("/kakao/**").permitAll()
-                      //  .anyRequest().authenticated()
+                        //.antMatchers("/naver/**").permitAll()
+                      //  .antMatchers("/kakao/**").permitAll()
+                        .anyRequest().authenticated()
                         );   // (무조건)증명
 
         // 2. 세션을 사용하지 않음
