2f74ac8633ab3e64b0984f38b4f4f49c009f1c58
sonwon <uoun456852@gmail.com>
2024-09-27 13:22:08 +0900

    FEAT:금융 팁 초기 생성

---
+++ src/main/java/fi/re/firebackend/controller/tip/TipController.java
@@ -0,0 +1,10 @@
+package fi.re.firebackend.controller.tip;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tip")
+public class TipController {
+    
+}
