f93a3540c699fc7d20be785885e71e9759999fb4
kUZEEwon <pink7519@pusan.ac.kr>
2024-10-02 15:44:03 +0900

    Feat: cart 로직 추가

---
+++ src/main/java/fi/re/firebackend/dto/login/UserResponseDto.java
@@ -0,0 +1,13 @@
+package fi.re.firebackend.dto.login;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponseDto {
+    private String username;
+    private String nickName;
+}
