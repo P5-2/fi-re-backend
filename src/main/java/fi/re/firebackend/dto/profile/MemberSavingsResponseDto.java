182b418613058914258b9074778839894bcd051c
oneul0 <gihoon1011@gmail.com>
2024-10-02 17:20:22 +0900

    Feat: profile의 membersaving(goal) 기능 추가
    - 전체 목표 진행률ì기능 추가
      > 가입한 예적금의 총 저축 금액 합산하여 상단 출력 기능 추가
    - 가입한 예적금 상품 리스트 진행률 확인 기ã능 추가
      > ê멤버가 가입한 상품을 각 상품별로 다음의 요소를 출력
    
         1. 상품명
         2. 현재 상품으로 현재까지 모은 금액
         3. 현재상품으로 에 이번달 입금한 금액
         4. 시작일
         5. 마감일
    
         6. 남은 기간
         7. ã은행명으로 은행 아이콘 출력

---
+++ src/main/java/fi/re/firebackend/dto/profile/MemberSavingsResponseDto.java
@@ -0,0 +1,26 @@
+package fi.re.firebackend.dto.profile;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MemberSavingsResponseDto { // 서버에서 응답해주는 내용
+    private String goalName; // 목표 이름
+    private String finPrdtCd; // 상품 코드(prdNo)
+    private String finPrdtNm; // 상품 이름
+    private String intrRateTypeNm; // 단리 복리
+    private Date startDate; // 가입일
+    private Date endDate; // 가입일
+    private double minRate;
+    private double maxRate;
+    private int savedAmount; // 지금까지 저금한 금액
+    private int monthlyDeposit; // 이번 달 입금 금액
+    private String bankname;
+}
